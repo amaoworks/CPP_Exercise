46746ae8f722c2cb0ab50065cde9052953f7210a
Brown <amaoworks@gmail.com>
2021-03-25 00:32:00 +0800

    add Ex1.7 & finish the chapter 1

--- Unit-01/Ex_1-7.cpp
+++ Unit-01/Ex_1-7.cpp
@@ -10,10 +10,38 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <fstream>
+
+using namespace std;
 
 int main()
 {
+    ifstream infile("D:\\Code\\CPP\\Unit-01\\base_data.txt");
+    ofstream outfile("D:\\Code\\CPP\\Unit-01\\out_data.txt");
+    vector<string> input_str;
+    string temp = {""};
 
+    if ((!infile) || (!outfile))
+        cerr << "Oops! Unable to open input file or output file! Please do more check!" << endl;
+    else
+    {
+        while (!infile.eof())
+        {
+            infile >> temp;
+            input_str.push_back(temp);
+        }
+    }
+
+    for(int i = 0; i < input_str.size(); ++i)
+    {
+        cout << input_str[i] << endl;
+    }
+
+    sort(input_str.begin(), input_str.end());
+
+    for (int j = 0; j < input_str.size(); ++j) {
+        outfile << input_str[j] << " ";
+    }
 
     return 0;
 }
